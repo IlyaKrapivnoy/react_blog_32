9bba25392a42d42fbeedd4fcbf04686d6ed54d42
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-03-01 15:16:30 +0200

    styled delete button

--- src/BlogList.js
+++ src/BlogList.js
@@ -7,13 +7,29 @@ const BlogList = ({ blogs, title, handleDelete }) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
-                    <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
+                    <button 
+                        onClick={() => handleDelete(blog.id)}
+                        style={deleteButtonStyles}
+                    >Delete</button>
                 </div>
             ))}
         </div>
      );
 }
- 
+
+const deleteButtonStyles = { 
+    background: '#f1356d', 
+    color: '#fff',
+    fontWeight: '500',
+    textTransform: 'uppercase',
+    letterSpacing: '2px',
+    border: 'none',
+    borderRadius: 15,
+    cursor: 'pointer',
+    margin: '15px 0',
+    padding: '2px 20px'
+}
+
 export default BlogList;
 
 
