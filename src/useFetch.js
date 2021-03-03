9cd8cdc58f06b3b788876d65479af3307e2dba30
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-03-04 00:07:06 +0200

    useEffect Cleanup

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,10 @@ const useFetch = (url) => {
     const [error, setError] = useState(null)
 
     useEffect(() => {
+        const abortCont = new AbortController()
+
         setTimeout(() => {
-           fetch(url)
+           fetch(url, { signal: abortCont.signal })
                .then(res => {
                    if(!res.ok) {
                        throw Error("hahah couldn't fetch the data from this resource")
@@ -20,11 +22,15 @@ const useFetch = (url) => {
                    setError(null)
                })
                .catch(err => {
-                   setIsLoading(false)
-                   setError(err.message)
+                   if (err.name === 'AbortError') {
+                       console.log('Fetch aborted')
+                   } else {
+                        setIsLoading(false)
+                        setError(err.message)
+                   }
                })
-
         }, 1000);
+        return () => abortCont.abort(); // it pauses our fetch
    }, [url]);  
 
     return { data, isLoading, error }
