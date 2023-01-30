9449a5f476a0b531afcd6e065b10d8228c3d7034
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2023-01-30 22:10:38 +0100

    refactor useFetch

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,39 +1,44 @@
 import { useState, useEffect } from 'react'
   
 const useFetch = (url) => {
-    const [data, setData] = useState(null)
-    const [isLoading, setIsLoading] = useState(true)
-    const [error, setError] = useState(null)
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        const abortCont = new AbortController()
+        let abortCont;
 
-        setTimeout(() => {
-           fetch(url, { signal: abortCont.signal })
-               .then(res => {
-                   if(!res.ok) {
-                       throw Error("hahah couldn't fetch the data from this resource")
-                   }
-                   return res.json()
-               })
-               .then(data => {
-                   setData(data)
-                   setIsLoading(false)
-                   setError(null)
-               })
-               .catch(err => {
-                   if (err.name === 'AbortError') {
-                       console.log('Fetch aborted')
-                   } else {
-                        setIsLoading(false)
-                        setError(err.message)
-                   }
-               })
-        }, 1000);
-        return () => abortCont.abort(); // it pauses our fetch
-   }, [url]);  
+        const fetchData = async () => {
+            abortCont = new AbortController();
 
-    return { data, isLoading, error }
+            try {
+                const res = await fetch(url, { signal: abortCont.signal });
+
+                if (!res.ok)
+                    throw Error(
+                        "hahah couldn't fetch the data from this resource"
+                    );
+
+                const data = await res.json();
+
+                setData(data);
+                setIsLoading(false);
+                setError(null);
+            } catch (err) {
+                if (err.name === 'AbortError') console.log('Fetch aborted');
+                else {
+                    setIsLoading(false);
+                    setError(err.message);
+                }
+            }
+        };
+
+        fetchData();
+
+        return () => abortCont && abortCont.abort();
+    }, [url]);
+
+    return { data, isLoading, error };
 }
 
 export default useFetch
