5a3503fa4700cf26e59e3a693e57858e2e70c004
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2023-01-30 22:07:17 +0100

    clear Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,14 @@
 import { Link } from 'react-router-dom'
 
-const Navbar = () => {
-    return (
-        <div>
-            <nav className="navbar">
-                <h1>Rude Boy's Blog</h1>
-                <div className="links">
-                    <Link to="/">Home</Link>
-                    <Link to="/create">New Blog</Link>
-                </div>
-            </nav>
+const Navbar = () => (
+    <nav className='navbar'>
+        <h1>Rude Boy's Blog</h1>
+        <div className='links'>
+            <Link to='/'>Home</Link>
+            <Link to='/create'>New Blog</Link>
         </div>
-    )
-}
+    </nav>
+);
+ 
 
 export default Navbar
