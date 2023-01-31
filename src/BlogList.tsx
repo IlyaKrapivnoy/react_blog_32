66786b15870c5ab38a5a8bbb7905235dbf3fc7c8
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2023-01-31 10:06:32 +0100

    refactor blog list

--- src/BlogList.tsx
+++ src/BlogList.tsx
@@ -12,23 +12,26 @@ interface Props {
 }
 
 const BlogList = ({ blogs, title }: Props) => {
+    const renderBlogs = () => {
+        return blogs.map((blog) => (
+            <Link to={`/blogs/${blog.id}`} key={blog.id}>
+                <div className='blog-preview'>
+                    <h2>{blog.title}</h2>
+
+                    <p>
+                        Written by{' '}
+                        <span style={{ color: '#949494' }}>{blog.author}</span>
+                    </p>
+                </div>
+            </Link>
+        ));
+    };
+
     return (
         <div className={'blog-list'}>
             <h2>{title}</h2>
-            {blogs.map((blog) => (
-                <Link to={`/blogs/${blog.id}`} key={blog.id}>
-                    <div className='blog-preview'>
-                        <h2>{blog.title}</h2>
-                        <p>
-                            Written by{' '}
-                            <span style={{ color: '#949494' }}>
-                                {' '}
-                                {blog.author}{' '}
-                            </span>
-                        </p>
-                    </div>
-                </Link>
-            ))}
+
+            {renderBlogs()}
         </div>
     );
 };
